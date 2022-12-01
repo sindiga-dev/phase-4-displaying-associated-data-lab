356c9ea36d6a72ec7a00aa82700c659fd8b4b856
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-12-01 22:11:46 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+    #Items GET /items returns an array of all items with user info
+    def index
+        @items = Item.all
+        render json: @items, include: :user
+    end
+
+    #Item GET /items/:id returns a single item with user info
+    def show
+        @item = Item.find(params[:id])
+        render json: @item, include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    # Users GET /users/:id returns a user with an array of all items associated with that user
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user, include: :items
+    end
 end
